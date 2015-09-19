20e3d0d8af093d7f8d9f5c4ca066c768a0e817dd
Atophite <wesley-reinders@outlook.com>
2015-09-19 20:26:01 +0200

    Change

--- WindowsApplication1/WindowsApplication1/Form1.Designer.vb
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -27,7 +27,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(113, 122)
+        Me.Button1.Location = New System.Drawing.Point(154, 59)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
